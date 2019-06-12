9ee6c8d1428385c0e136a53936e094161db906d9
Phlammariont <39630248+Phlammariont@users.noreply.github.com>
2019-06-12 17:28:30 -0500

    purchase and inventory model (#7)
    * purchase and inventory model
    
    * fixing pr comments

---
+++ src/model/inventory-item.js
@@ -0,0 +1,15 @@
+import Model from '../components/crud/model'
+import Product from './product'
+
+class InventoryItem extends Model {
+  name = 'Inventario'
+  fields = [
+    { name: 'uid', label: 'Id', isHide: true },
+    { name: 'product', label: 'Productos', instanceOf: Product },
+    { name: 'value', label: 'Valor' },
+    { name: 'fabric', label: 'Tela' },
+    { name: 'color', label: 'Color' },
+  ]
+}
+
+export default InventoryItem
