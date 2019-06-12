9599c2b0e37504e2fdf736888759294ae1aa6206
Leon Rueda <p.rueda-gomez@klarna.com>
2019-06-13 00:25:53 +0200

    purchase and inventory model

---
+++ src/model/purchase.js
@@ -0,0 +1,15 @@
+import Model from '../components/crud/model'
+import Customer from './customer'
+import InventoryItem from './inventory-item'
+
+class Purchase extends Model {
+  name = 'Negocio'
+  fields = [
+    { name: 'uid', label: 'Id', isHide: true },
+    { name: 'customer', label: 'Cliente', instanceOf: Customer },
+    { name: 'products', label: 'Productos', instanceOf: [InventoryItem] },
+    { name: 'total', label: 'Total' },
+  ]
+}
+
+export default Purchase
